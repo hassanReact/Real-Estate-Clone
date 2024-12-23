13c3beaf3e4f65f42b82b8723c4150279edff058
Hassan Kashif <hassank1751@gmail.com>
2024-12-23 03:39:13 -0800

    4th commit

--- src/app/user/properties/add/_components/Image.tsx
+++ src/app/user/properties/add/_components/Image.tsx
@@ -1,4 +1,5 @@
-import { Card, cn } from '@nextui-org/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/16/solid';
+import { Button, Card, cn } from '@nextui-org/react';
 import React from 'react'
 
 interface Props {
@@ -7,11 +8,23 @@ interface Props {
     className?: string;
 }
 const Image = (props:Props) => {
-    const handleNexts =() => props.next()
-    const handlePrevious = () => props.prev()
+    const handleNext =() => props.next()
+    const handlePrev = () => props.prev()
   return (
     <Card className={cn("p-2 grid grid-cols-1 md:grid-cols-2 gap-3", props.className)}>
-      
+        <div className='mt-3 flex justify-center col-span-2 gap-3'>
+      <Button 
+       onPress={handlePrev}
+    startContent={<ChevronLeftIcon className='w-6'/>} 
+      color='primary' 
+      className='w-36'>
+        Previous
+      </Button>
+      <Button onPress={handleNext} endContent={<ChevronRightIcon className='w-6'/>} 
+       color='primary' className='w-36'>
+        Next
+      </Button>
+    </div>
     </Card>
   )
 }
